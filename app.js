2dff95c29f2fa451f9a5828b9571b296acd4890d
AinunAbidin <ainun.ilabaca@gmail.com>
2022-02-18 07:07:20 +0700

    getIP

--- app.js
+++ app.js
@@ -18,9 +18,16 @@ app.set('views',view_path)
 
 
 app.use(requestIp.mw())
+app.use(function(req, res, next) {
+  var ip_info = getIP(req);
+  console.log(ip_info);
+  // { clientIp: '127.0.0.1', clientIpRoutable: false }
+  next();
+});
 
 app.use(express.static(public_path));
 app.get('/', function (req, res) {
+  console.log(req.header)
   var ipaddress = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress).split(",")[0];
   res.render('index', {
     ip: ipaddress
